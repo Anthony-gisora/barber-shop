9ec01c46586be80a607d6d72e2e71b859493a725
Anthony-Gisemba <99672074+Anthony-gisora@users.noreply.github.com>
2025-03-18 15:27:24 +0300

    Update Card.jsx
    Added some Tailwind code to the Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 const Card = () => {
     return ( 
         <div className=" m-2 h-[98%]">
-            <h1>1</h1>
+            <h1 className= "flex flex-col justify-centre items-centre">Welcome </h1>
         </div>
      );
 }
